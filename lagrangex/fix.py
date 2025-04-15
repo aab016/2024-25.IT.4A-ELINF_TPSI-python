4867ab4e245e9a06c294c17cd212a71348f50260
aab016 <prof.alessandro_aglietti@iislagrange.edu.it>
2025-04-15 22:17:11 +0000

    fix: real order book data

--- lagrangex/fix.py
+++ lagrangex/fix.py
@@ -1,6 +1,8 @@
 from termcolor import colored
 from threading import Thread, Event
 import uuid
+import csv
+import os
 
 SPEED_SLOW = 5.0
 SPEED_NORMAL = 1.0
@@ -12,28 +14,35 @@ _fix_thread = None
 
 class MyFixThread(Thread):
     
-    def __init__(self, event, orderbook_handler_speed, orderbook_handler):
+    def __init__(self, event, orderbook_handler_speed, orderbook_handler, orderbook_depth_level = "1"):
         Thread.__init__(self)
         self.stopped = event
         self._orderbook_handler_speed = orderbook_handler_speed
         self._orderbook_handler = orderbook_handler
+        self._orderbook_depth_level = orderbook_depth_level
+        csv_file_name = "AMZN_2012-06-21_34200000_57600000_orderbook_{}.csv".format(self._orderbook_depth_level)
+        current_working_directory = os.path.dirname(os.path.realpath(__file__))
+        csv_path = "{}/../data/{}".format(current_working_directory, csv_file_name)
+        csvfile = open(csv_path, newline='\n')
+        self._spamreader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
 
     def run(self):
         while not self.stopped.wait(self._orderbook_handler_speed):
             orderbook_update_id = 123
+            orderbook_update_data_next = self._spamreader.__next__()
             orderbook_update_data = {
                 "SYMBOLS": {
                     "AMZN": {
                         "BID": [
                             {
-                                "PRICE": 1234,
-                                "QUANTITY": 4567
+                                "PRICE": int(orderbook_update_data_next["BID_1_PRICE"]),
+                                "QUANTITY": int(orderbook_update_data_next["BID_1_QTY"])
                             }
                         ],
                         "ASK": [
                             {
-                                "PRICE": 1234,
-                                "QUANTITY": 4567
+                                "PRICE": int(orderbook_update_data_next["ASK_1_PRICE"]),
+                                "QUANTITY": int(orderbook_update_data_next["ASK_1_QTY"])
                             }
                         ]
                     }
